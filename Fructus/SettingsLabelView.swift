589d5b2837aea8a3239cb662f2ea920ac5805c91
p0nyqz <ponyquiz@gmail.com>
2022-05-11 00:38:41 +0200

    SettingsLabelView & First section added

---
+++ Fructus/SettingsLabelView.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsLabelView.swift
+//  Fructus
+//
+//  Created by p0nyqz on 11.05.2022.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    
+    // MARK: - PROPERTIES
+    
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
