335f5d95f41136800fa00f752c1be662d780a2e4
p0nyqz <ponyquiz@gmail.com>
2022-05-10 20:49:45 +0200

    NutrientsView embedded to DetailView

--- Fructus/FruitNutrientsView.swift
+++ Fructus/FruitNutrientsView.swift
@@ -14,8 +14,29 @@ struct FruitNutrientsView: View {
     
     //MARK: - BODY
     
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox() {
+            DisclosureGroup("Nutritional value per 100g") {
+                ForEach(0..<nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
+                    
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(Font.system(.body).bold())
+
+                        Spacer(minLength: 25)
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+            }
+        } //: BOX
     }
 }
 
