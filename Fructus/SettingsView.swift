3fa8909c447a54a0cc1524cf2822dbf113eb866f
p0nyqz <ponyquiz@gmail.com>
2022-05-12 15:00:35 +0200

    Customization Section Added

--- Fructus/SettingsView.swift
+++ Fructus/SettingsView.swift
@@ -42,6 +42,19 @@ struct SettingsView: View {
                     
                     // MARK: - SECTION 2
                     
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ){
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding precess and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                    }
+                    
                     // MARK: - SECTION 3
                     
                     GroupBox(
